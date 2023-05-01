5cfc4a066da352fcb65d2dadf8388e363ebb4d66
Pushpak Khadke <100704509+PushpakKhadke@users.noreply.github.com>
2023-05-01 14:50:18 +0530

    Add files via upload

---
+++ Projects/LED-BLINKING-3/LED-BLINKING-3.ino
@@ -0,0 +1,18 @@
+int ledPin = 13; // LED connected to digital pin 13
+int buttonPin = 2; // push button connected to digital pin 2
+int buttonState = 0; // variable to store the button state
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // initialize the LED pin as an output
+  pinMode(buttonPin, INPUT); // initialize the button pin as an input
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin); // read the state of the button
+  if (buttonState == HIGH) { // if the button is pressed
+    digitalWrite(ledPin, HIGH); // turn on the LED
+  } else { // if the button is not pressed
+    digitalWrite(ledPin, LOW); // turn off the LED
+  }
+}
+
